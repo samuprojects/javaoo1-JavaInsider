2b0fc860a5f918a83d113b250f87ae8890ac3bc5
samuel <samugit.projects@gmail.com>
2023-06-19 18:26:40 -0300

    Aula02 Classe com atributos

---
+++ App.java
@@ -0,0 +1,10 @@
+public class App {
+
+    public static void main(String[] args) {
+
+        Account a1 = new Account();
+        a1.accountNumber = "1234-5";
+
+        System.out.println(a1.accountNumber);
+    }
+}
