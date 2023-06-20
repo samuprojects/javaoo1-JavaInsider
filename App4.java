4bad069883037200f443aec8cfbc7a0f51824e5e
samuel <samugit.projects@gmail.com>
2023-06-20 15:34:04 -0300

    Aula11 Inicialização de atributos em objetos

---
+++ App4.java
@@ -0,0 +1,9 @@
+public class App4 {
+
+    public static void main(String[] args) {
+
+        Account a = new Account();
+        a.printBalance();
+        System.out.println(a.accountNumber);
+    }
+}
