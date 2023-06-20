e2d5b738df3fc94a611569aa30182c8998189732
samuel <samugit.projects@gmail.com>
2023-06-20 11:16:27 -0300

    Aula06 Métodos em uma classe

--- Account.java
+++ Account.java
@@ -3,4 +3,12 @@ public class Account {
     String accountNumber;
     String accountOwner;
     double balance;
+
+    void deposit(double amount) {
+        balance += amount;
+    }
+
+    void printBalance() {
+        System.out.println("Balance: R$ " + balance);
+    }
 }
---
+++ App2.java
@@ -0,0 +1,9 @@
+public class App2 {
+    public static void main(String[] args) {
+
+        Account a = new Account();
+        a.deposit(100);
+        a.deposit(200);
+        a.printBalance();
+    }
+}
