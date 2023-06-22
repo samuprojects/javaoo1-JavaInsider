90f81e562d741bec448c498348437eb27aabfdb1
samuel <samugit.projects@gmail.com>
2023-06-22 13:01:27 -0300

    Aula22 Usando o modificador final em atributos Parte2

--- App5.java
+++ App5.java
@@ -2,14 +2,14 @@ public class App5 {
 
     public static void main(String[] args) {
 
-        var a = new EncapsulatedAccount();
+        var a = new EncapsulatedAccount("12345", 0);
         a.deposit(1000);
         a.withdraw(200);
         a.printBalance();
 
         System.out.println(a.getAccountNumber());
 
-        a.setAccountOwner("Pedro");
+       // a.setAccountOwner("Pedro");
         System.out.println(a.getAccountOwner());
     }
 }
--- App6.java
+++ App6.java
@@ -6,6 +6,9 @@ public class App6 {
         final var account1 = new EncapsulatedAccount("1234"); // Variável está imutável
         final var account2 = new EncapsulatedAccount("4321", 1000);
 
-        // account1 = new EncapsulatedAccount("3456"); ERRO -> Cannot assign a value to final variable 'account1'
+        account1.printBalance();
+        account1.deposit(1000); // cai na prova!!!!
+        account1.printBalance(); // é possível pois o objeto pode ser alterado, o que não pode mudar
+        // é a referência desse objeto na variável que está na stack esse é final
     }
 }
