82abceb5995e3d1b43d4b9cab8aebfbfd833ae78
samuel <samugit.projects@gmail.com>
2023-06-22 12:34:56 -0300

    Aula20 Construtores que chamam construtores

--- EncapsulatedAccount.java
+++ EncapsulatedAccount.java
@@ -10,16 +10,24 @@ public class EncapsulatedAccount {
     public EncapsulatedAccount(){
     }
 
-    public EncapsulatedAccount(String accountNumber){
+    public EncapsulatedAccount(String accountNumber, String accountOwner, double balance) {
         this.accountNumber = accountNumber;
-    }
-
-    public EncapsulatedAccount(String accountNumber, double balance){
-        this.accountNumber = accountNumber;
+        this.accountOwner = accountOwner;
         this.balance = balance;
     }
 
+    public EncapsulatedAccount(String accountNumber){
+        //System.out.println(); ERRO -> Call to 'this()' must be first statement in constructor body
+        this(accountNumber,null,0); // como o objeto não foi construído ainda não pode chamar nada antes do this
+    }
 
+    public EncapsulatedAccount(String accountNumber, String accountOwner){
+        this(accountNumber, accountOwner,0);
+    }
+
+    public EncapsulatedAccount(String accountNumber, double balance){
+        this(accountNumber, null, balance);
+    }
 
     boolean deposit(double amount) {
         if (amount > 0) {
