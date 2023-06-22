8070adfee432c35b80d2140927b5107eb7f5b150
samuel <samugit.projects@gmail.com>
2023-06-22 11:09:01 -0300

    Aula16 Criando uma classe pública

---
+++ SomeClass.java
@@ -0,0 +1,8 @@
+@SuppressWarnings("ALL")
+public class SomeClass {
+    // visibilidade pública (apenas uma por arquivo) também chamada de top level class,
+    // ou seja, definida diretamente dentro do arquivo .java que leva o mesmo nome da classe
+}
+
+// public class D{} ERRO -> Class 'D' is public, should be declared in a file named 'D.java'
+
