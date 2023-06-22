be9da64736f7c569d0cb7ec104c1046f17a5b5df
samuel <samugit.projects@gmail.com>
2023-06-22 10:21:30 -0300

    Aula14 Exercício criar fração e multiplicar outra

---
+++ ExFraction.java
@@ -0,0 +1,16 @@
+public class ExFraction {
+
+    public static void main(String[] args) {
+
+        Fraction f1 = new Fraction();
+        f1.set(2, 4);
+        f1.show();
+
+        Fraction f2 = new Fraction();
+        f2.set(3, 6);
+        f2.show();
+
+        Fraction f3 = f1.multiply(f2);
+        f3.show();
+    }
+}
---
+++ Fraction.java
@@ -0,0 +1,22 @@
+public class Fraction {
+
+    int n;
+    int d;
+
+    void set (int n, int d) {
+        this.n = n;
+        this.d = d;
+
+    }
+
+    Fraction multiply(Fraction f){
+        Fraction r = new Fraction();
+        r.n = n * f.n;
+        r.d = d * f.d;
+        return r;
+    }
+
+    void show(){
+        System.out.println(n + " / " + d);
+    }
+}
