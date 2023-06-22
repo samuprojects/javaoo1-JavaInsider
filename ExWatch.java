99152c0f6deaddc7a8778177507b5e26d3d4d26a
samuel <samugit.projects@gmail.com>
2023-06-22 10:54:20 -0300

    Aula15 Exercício implementar um relógio

---
+++ ExWatch.java
@@ -0,0 +1,14 @@
+public class ExWatch {
+
+    public static void main(String[] args) {
+
+        Watch w = new Watch();
+        System.out.println("A hora informada foi");
+        w.setTime(11,30,40);
+        System.out.println(w.readHour() + ":" + w.readMinute() + ":" + w.readSecond());
+        System.out.println("O ponteiro das horas está no " + w.hour.number);
+        System.out.println("O ponteiro dos minutos está no " + w.minute.number);
+        System.out.println("O ponteiro dos segundos está no " + w.second.number);
+
+    }
+}
---
+++ Watch.java
@@ -0,0 +1,26 @@
+@SuppressWarnings("ALL")
+public class Watch {
+
+    Pointer hour = new Pointer();
+    Pointer minute = new Pointer();
+    Pointer second = new Pointer();
+
+    void setTime(int hour, int minute, int second) {
+        this.hour.number = hour;
+        this.minute.number = minute / 5;
+        this.second.number = second / 5;
+    }
+
+    int readHour(){
+        return hour.number;
+    }
+
+    int readMinute(){
+        return minute.number * 5;
+
+    }
+
+    int readSecond(){
+        return second.number * 5;
+    }
+}
