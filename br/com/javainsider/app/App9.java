ea6912b33126304676f845520333a6e0d6fcf417
samuel <samugit.projects@gmail.com>
2023-06-28 15:51:48 -0300

    Aula36 Importando classes de outros pacotes - parte2

--- br/com/javainsider/app/App9.java
+++ br/com/javainsider/app/App9.java
@@ -1,6 +1,8 @@
 package br.com.javainsider.app;
 
-import br.com.javainsider.account.Account;
+import br.com.javainsider.account.*; // Posso usar o Fully Qualified, ou imports diretos, ou o *
+// no intellij o atalho CrtL + Alt + O a IDE já organiza os imports em ordem alfabética
+// ou pelo menu Code Optimize Imports
 
 public class App9 {
 
@@ -11,6 +13,7 @@ public class App9 {
         Account a = new Account();
         Account b = new Account();
         Account c = new Account();
+        EncapsulatedAccount d = new EncapsulatedAccount("1234");
     }
 
 }
