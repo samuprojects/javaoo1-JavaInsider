8b78d489cf54bd2fb484f86571dd5b587110117f
samuel <samugit.projects@gmail.com>
2023-06-28 15:41:30 -0300

    Aula35 Importando classes de outros pacotes - parte1

---
+++ br/com/javainsider/app/App9.java
@@ -0,0 +1,18 @@
+package br.com.javainsider.app;
+
+import br.com.javainsider.account.Account;
+
+public class App9 {
+
+    @SuppressWarnings("ALL")
+    public static void main(String[] args) {
+
+        // Fully Qualified Name
+        Account a = new Account();
+        Account b = new Account();
+        Account c = new Account();
+    }
+
+}
+
+
